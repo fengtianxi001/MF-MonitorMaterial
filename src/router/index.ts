783b13cb4c2fa2b3454798386f53c93c560aeeea
fengtianxi001 <625362207@qq.com>
2025-02-11 15:22:13 +0800

    first commit

---
+++ src/router/index.ts
@@ -0,0 +1,49 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import { Layout } from '@/layout'
+
+export const routes: any[] = [
+  {
+    path: '/title',
+    name: 'title',
+    meta: { title: '标题素材' },
+    component: () => import('@/pages/title/index.vue'),
+  },
+  {
+    path: '/border',
+    name: 'border',
+    meta: { title: '边框素材' },
+    component: () => import('@/pages/border/index.vue'),
+  },
+  {
+    path: '/chart',
+    name: 'chart',
+    meta: { title: '图标素材' },
+    component: () => import('@/pages/chart/index.vue'),
+  },
+  {
+    path: '/fonts',
+    name: 'fonts',
+    meta: { title: '字体素材' },
+    component: () => import('@/pages/title/index.vue'),
+  },
+  {
+    path: '/bgi',
+    name: 'bgi',
+    meta: { title: '背景素材' },
+    component: () => import('@/pages/border/index.vue'),
+  },
+]
+
+export const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      redirect: '/title',
+      component: Layout,
+      children: [...routes],
+    },
+  ],
+})
+
+export default router
